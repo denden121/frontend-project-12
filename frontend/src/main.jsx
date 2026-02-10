daf9327400321916054407a219672fcac7b966c8
dgurachevskii <lisnik.job@gmail.com>
2026-02-10 13:07:16 +0300

    Исравил

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,24 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App.jsx';
 import store from './store/index';
 import './i18n';
-import rollbarInstance, { isRollbarEnabled } from './rollbarConfig';
+import './rollbarConfig';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RollbarProvider instance={rollbarInstance} enabled={isRollbarEnabled}>
-      <ErrorBoundary>
-        <Provider store={store}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Provider>
-      </ErrorBoundary>
-    </RollbarProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
